ded8cedc24074b83241b994d5700fc157be620aa
Liuabi520 <112585382+Liuabi520@users.noreply.github.com>
2022-11-25 17:36:24 -0500

    Tony's copy main

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
@@ -5,6 +7,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,7 +17,9 @@ class User(db.Model):
     def __repr__(self):
         return '<Login %r>' % self.id
 
+
 class Passenger(db.Model):
+    __tablename__ = 'passenger'
     d_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), default='')
     phoneNumber = db.Column(db.String(200), default='')
@@ -25,25 +30,41 @@ class Passenger(db.Model):
     def __repr__(self):
         return '<Login %r>' % self.d_id
 
+
 class Driver(db.Model):
+    __tablename__ = 'driver'
     d_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), default='')
     phoneNumber = db.Column(db.String(200), default='')
     address = db.Column(db.String(200), default='')
     card_info = db.Column(db.String(200), default='')
+
     # todo: add car's info to Driver
     def __repr__(self):
         return '<Login %r>' % self.d_id
 
 
-@app.route('/', methods=['POST','GET'])
+class Appointment(db.Model):
+    a_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    d_id = db.Column(db.Integer, db.ForeignKey('driver.d_id'))
+    p_id = db.Column(db.Integer, db.ForeignKey('passenger.d_id'))
+    planned_pickup = db.Column(db.String(200), default='')
+    planned_destination = db.Column(db.String(200), default='')
+    planned_start_time = db.Column(db.String(200), default='')
+    planned_payment_amount = db.Column(db.String(200), default='')
+
+    def __repr__(self):
+        return '<Login %r>' % self.a_id
+
+
+@app.route('/', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        if(request.form['btn'] == 'login'):
+        if (request.form['btn'] == 'login'):
             login_id = request.form['id']
             login_pw = request.form['password']
             exists = db.session.query(db.exists().where(User.id == login_id)).scalar()
-            if(not exists):
+            if (not exists):
                 flash('Error: id not exists')
                 return redirect('/')
 
@@ -66,12 +87,13 @@ def login():
     else:
         return render_template('login.html')
 
-@app.route('/homepage_p/<int:id>',methods=['POST', 'GET'])
+
+@app.route('/homepage_p/<int:id>', methods=['POST', 'GET'])
 def homepage_p(id):
-    if request.method=='GET':
+    if request.method == 'GET':
         passenger = Passenger.query.get_or_404(id)
-        return render_template('homepage_passenger.html',tasks=passenger)
-    elif request.method=='POST':
+        return render_template('homepage_passenger.html', tasks=passenger)
+    elif request.method == 'POST':
         passenger = Passenger.query.get_or_404(id)
         passenger.name = request.form['name']
         passenger.phoneNumber = request.form['phoneNumber']
@@ -85,7 +107,7 @@ def homepage_p(id):
             return "something wrong"
 
 
-@app.route('/homepage_d/<int:id>',methods=['POST', 'GET'])
+@app.route('/homepage_d/<int:id>', methods=['POST', 'GET'])
 def homepage_d(id):
     if request.method == 'GET':
         driver = Driver.query.get_or_404(id)
@@ -103,9 +125,10 @@ def homepage_d(id):
         except:
             return "something wrong"
 
+
 @app.route("/register", methods=['POST', 'GET'])
 def index():
-    if request.method=='POST':
+    if request.method == 'POST':
         login_id = int(request.form['id'])
         login_pw = request.form['password']
         login_isDriver = request.form['isDriver']
@@ -134,9 +157,10 @@ def index():
         logins = User.query.order_by(User.id).all()
         return render_template('index.html', tasks=logins)
 
-@app.route('/admin',methods=['POST', 'GET'])
+
+@app.route('/admin', methods=['POST', 'GET'])
 def admin():
-    if request.method=='POST':
+    if request.method == 'POST':
         login_id = request.form['id']
         login_pw = request.form['password']
         login_isDriver = request.form['isDriver']
@@ -172,5 +196,37 @@ def delete(id):
         return 'There was a problem deleting that registration'
 
 
-if __name__=="__main__":
+@app.route('/<int:id>/appointment', methods=['POST', 'GET'])
+def appointment(id):
+    if request.method == 'POST':
+        appointment_pickup = request.form['pickup']
+        appointment_dest = request.form['destination']
+        appointment_order_time = str(datetime.now())
+        appointment_pltime = request.form['pltime']
+        appointment_ppay = request.form['plpay']
+        order = request.form['btn']
+        appoint = request.form['btn']
+        if order == "Order Now!":
+            new_appointment = Appointment(planned_pickup=appointment_pickup, planned_destination=appointment_dest,
+                                          planned_start_time=appointment_order_time,
+                                          planned_payment_amount=appointment_ppay, p_id=id)
+        elif appoint == "Make an Appointment now!":
+            new_appointment = Appointment(planned_pickup=appointment_pickup, planned_destination=appointment_dest,
+                                          planned_start_time=appointment_pltime,
+                                          planned_payment_amount=appointment_ppay, p_id=id)
+        try:
+            db.session.add(new_appointment)
+            db.session.commit()
+            flash("updated")
+            return render_template('appointment.html', tasks=new_appointment)
+        except:
+            return render_template('appointment.html', tasks=new_appointment)
+    else:
+        appointment = Appointment.query.get_or_404(id)
+        return render_template('appointment.html', tasks=appointment)
+
+
+if __name__ == "__main__":
+    app.app_context().push()
+    db.create_all()
     app.run(debug=True)
